afbcc1d7d2055c9a79b80fabb80a976dba061299
malinda-sampath <malindasampath45@gmail.com>
2026-09-09 01:30:03 +0530

    Python syntax

---
+++ w3schools/2_syntax.py
@@ -0,0 +1,11 @@
+if 5 > 2:
+    print("5 is greater than 2")
+
+# This is a comment
+print("This will always be printed.", end="")
+print(" This is on the same line as the previous print statement.")
+
+# Numbers and arithmetic operations
+print(500)
+print(5*6)
+print("Text", 5*6, "more text")
