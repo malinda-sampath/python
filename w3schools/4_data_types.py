2333606e52f0612458d31d4e043eaf18ba2c7777
malinda-sampath <malindasampath45@gmail.com>
2026-09-14 00:53:50 +0530

    Python Data Types

---
+++ w3schools/4_data_types.py
@@ -0,0 +1,42 @@
+# str data type
+a = "Hello, World!"
+
+# int data types
+b = 20
+
+# float data type
+c = 20.5
+
+# list data type
+d = ["apple", "banana", "cherry"]
+
+# tuple data type
+e = ("apple", "banana", "cherry")
+
+# range data type
+f = range(6)
+
+# dict data type
+g = {"name" : "John", "age" : 36}
+
+# set data type
+h = {"apple", "banana", "cherry"}
+
+# frozenset data type
+i = frozenset({"apple", "banana", "cherry"})
+
+# bool data type
+j = True
+
+# bytes data type
+k = b"Hello"
+
+# bytearray data type
+l = bytearray(5)
+
+# memoryview data type
+m = memoryview(bytes(5))
+
+# None data type
+n = None
+
