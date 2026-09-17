5bc0131a03946f60f4198dc64cea4fa775813fed
malinda-sampath <malindasampath45@gmail.com>
2026-09-18 00:42:05 +0530

    Python Numbers

---
+++ w3schools/1_python_tutorial/5_python_numbers.py
@@ -0,0 +1,44 @@
+x = 1
+y = 2.8
+z = 1j
+
+
+# int
+a = 1 
+b = 62356253472583764523
+c = -3255522
+
+# int type conversion
+j = float(a) # convert int to float
+k = complex(a) # convert int to complex
+
+print("Int type conversion:")
+print(j)
+print(k)
+
+# float
+d = 1.10
+e = 1.0
+f = -35.59
+
+# float type conversion
+l = int(d) # convert float to int
+m = complex(d) # convert float to complex
+
+print("Float type conversion:")
+print(l)
+print(m)
+
+# complex
+g = 3+14j
+h = 5j
+i = -5j
+
+# complex type conversion
+n = int(g) # convert complex to int 
+o = float(g) # convert complex to float
+
+print("Complex type conversion:")
+print(n)
+print(o)
+# You cannot convert complex numbers into another number type. You can only convert complex numbers into complex numbers.
